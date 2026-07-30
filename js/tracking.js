bf3bf356d42d3aada731c731736244d1dc6b82fc
naimoricle-eng <naim.oricle@gmail.com>
2026-07-30 18:01:00 +0800

    Refactor tracking functions and improve accuracy

--- js/tracking.js
+++ js/tracking.js
@@ -1,45 +1,124 @@
 import { db } from "./firebase.js";
-import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+collection,
+addDoc,
+serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 let watchID = null;
+let lastLat = null;
+let lastLon = null;
 
-export function startTracking(userId){
+function getDistance(lat1, lon1, lat2, lon2){
 
-if(watchID !== null) return;
+const R = 6371000;
 
-watchID = navigator.geolocation.watchPosition(
-async (position)=>{
+const dLat = (lat2-lat1) * Math.PI/180;
+const dLon = (lon2-lon1) * Math.PI/180;
 
-const lat = position.coords.latitude;
-const lng = position.coords.longitude;
+const a =
+Math.sin(dLat/2)**2 +
+Math.cos(lat1*Math.PI/180) *
+Math.cos(lat2*Math.PI/180) *
+Math.sin(dLon/2)**2;
 
-console.log("Tracking:", lat, lng);
-
-await addDoc(collection(db,"tracking"),{
- userId:userId,
- latitude:lat,
- longitude:lng,
- time:serverTimestamp()
-});
-
-},
-(error)=>{
-console.log("GPS Error:",error.message);
-},
-{
-enableHighAccuracy:true,
-maximumAge:5000,
-timeout:10000
-});
+return R * 2 * Math.atan2(
+Math.sqrt(a),
+Math.sqrt(1-a)
+);
 
 }
 
+export function startTracking(){
+
+if(watchID!==null) return;
+
+watchID = navigator.geolocation.watchPosition(
+
+async(position)=>{
+
+const attendanceID =
+localStorage.getItem("attendanceID");
+
+if(!attendanceID) return;
+
+const lat = position.coords.latitude;
+const lon = position.coords.longitude;
+
+if(lastLat!==null){
+
+const meter =
+getDistance(
+lastLat,
+lastLon,
+lat,
+lon
+);
+
+if(meter<20){
+
+return;
+
+}
+
+}
+
+lastLat = lat;
+lastLon = lon;
+
+await addDoc(
+
+collection(
+db,
+"attendance",
+attendanceID,
+"tracking"
+),
+
+{
+
+latitude:lat,
+longitude:lon,
+time:serverTimestamp()
+
+}
+
+);
+
+console.log("Tracking:",lat,lon);
+
+},
+
+(error)=>{
+
+console.log(error.message);
+
+},
+
+{
+
+enableHighAccuracy:true,
+maximumAge:5000,
+timeout:10000
+
+}
+
+);
+
+}
 
 export function stopTracking(){
 
-if(watchID !== null){
+if(watchID!==null){
+
 navigator.geolocation.clearWatch(watchID);
+
 watchID=null;
+
+lastLat=null;
+lastLon=null;
+
 }
 
 }
